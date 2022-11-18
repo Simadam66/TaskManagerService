c0123b88b29cddae085580a6db880ec8c4faba4c
Ádám János Simon <adam.simon@gobeyond.aero>
2022-11-18 13:01:39 +0100

    Schema examples added

--- src/main/java/com/example/demo/controller/TaskController.java
+++ src/main/java/com/example/demo/controller/TaskController.java
@@ -1,17 +1,28 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.UserResponse;
 import com.example.demo.model.task.Task;
 import com.example.demo.dto.TaskRequest;
 import com.example.demo.dto.TaskResponse;
 import com.example.demo.service.TaskService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
+@Tag(name = "Task Controller", description = "This controller is responsible for any operation in connection with the tasks.")
 @RestController
 @RequestMapping(path = "api/v1/user/{userId}/task")
 public class TaskController {
@@ -23,6 +34,10 @@ public class TaskController {
         this.taskService = taskService;
     }
 
+    @Operation( summary = "Get all Tasks of a User by the its Id",
+            parameters = { @Parameter(in = ParameterIn.PATH, name = "userId", description = "Id of the User", example = "1") },
+            responses = { @ApiResponse(responseCode = "200", description = "Successful operation",content = {@Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = TaskResponse.class)))}),
+                          @ApiResponse(responseCode = "404", description = "Can not find User", content = @Content(schema = @Schema(hidden = true)))})
     @GetMapping
     public ResponseEntity<List<TaskResponse>> getUserTasks(@PathVariable("userId") Long userId) {
         return ResponseEntity.ok(
@@ -33,12 +48,24 @@ public class TaskController {
         );
     }
 
+    @Operation( summary = "Get a one of a Users Tasks",
+            parameters = { @Parameter(in = ParameterIn.PATH, name = "userId", description = "Id of the User", example = "1"),
+                           @Parameter(in = ParameterIn.PATH, name = "taskId", description = "Id of the Task", example = "3")},
+            responses = { @ApiResponse(responseCode = "200", description = "Successful operation", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = TaskResponse.class))),
+                          @ApiResponse(responseCode = "404", description = "Can not find User", content = @Content(schema = @Schema(hidden = true))),
+                          @ApiResponse(responseCode = "404", description = "Can not find Task", content = @Content(schema = @Schema(hidden = true))),
+                          @ApiResponse(responseCode = "400", description = "The requested Task does not belong to the User", content = @Content(schema = @Schema(hidden = true)))})
     @GetMapping(path = "{taskId}")
     public ResponseEntity<TaskResponse> getTask(@PathVariable("userId") Long userId,
                                                 @PathVariable("taskId") Long taskId) {
         return ResponseEntity.ok(TaskResponse.of(taskService.getTask(userId, taskId)));
     }
 
+    @Operation( summary = "Add a new Task to a User",
+            parameters = { @Parameter(in = ParameterIn.PATH, name = "userId", description = "Id of the User", example = "1") },
+            responses = { @ApiResponse(responseCode = "200", description = "Successful operation", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = TaskResponse.class))),
+                          @ApiResponse(responseCode = "404", description = "Can not find User", content = @Content(schema = @Schema(hidden = true))),
+                          @ApiResponse(responseCode = "400", description = "Invalid request-body", content = @Content(schema = @Schema(hidden = true)))})
     @PostMapping
     public ResponseEntity<TaskResponse> registerNewTask(@PathVariable("userId") Long userId,
                                                         @RequestBody @Valid TaskRequest taskRequest) {
@@ -47,6 +74,13 @@ public class TaskController {
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
+    @Operation( summary = "Delete a Task of a User",
+            parameters = { @Parameter(in = ParameterIn.PATH, name = "userId", description = "Id of the User", example = "1"),
+                           @Parameter(in = ParameterIn.PATH, name = "taskId", description = "Id of the Task", example = "3")},
+            responses = { @ApiResponse(responseCode = "200", description = "Successful operation", content = @Content(schema = @Schema(hidden = true))),
+                          @ApiResponse(responseCode = "404", description = "Can not find User", content = @Content(schema = @Schema(hidden = true))),
+                          @ApiResponse(responseCode = "404", description = "Can not find Task", content = @Content(schema = @Schema(hidden = true))),
+                          @ApiResponse(responseCode = "400", description = "The requested Task does not belong to the User", content = @Content(schema = @Schema(hidden = true)))})
     @DeleteMapping(path = "{taskId}")
     public ResponseEntity<String> deleteTask(@PathVariable("userId") Long userId,
                                              @PathVariable("taskId") Long taskId) {
@@ -54,6 +88,14 @@ public class TaskController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @Operation( summary = "Update a User",
+            parameters = { @Parameter(in = ParameterIn.PATH, name = "userId", description = "Id of the User", example = "1"),
+                           @Parameter(in = ParameterIn.PATH, name = "taskId", description = "Id of the Task", example = "3")},
+            responses = { @ApiResponse(responseCode = "200", description = "Successful operation", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = TaskResponse.class))),
+                          @ApiResponse(responseCode = "404", description = "Can not find User", content = @Content(schema = @Schema(hidden = true))),
+                          @ApiResponse(responseCode = "404", description = "Can not find Task", content = @Content(schema = @Schema(hidden = true))),
+                          @ApiResponse(responseCode = "400", description = "The requested Task does not belong to the User", content = @Content(schema = @Schema(hidden = true))),
+                          @ApiResponse(responseCode = "400", description = "Invalid request-body", content = @Content(schema = @Schema(hidden = true)))})
     @PutMapping(path = "{taskId}")
     public ResponseEntity<TaskResponse> updateTask(
             @PathVariable("userId") Long userId,
--- src/main/java/com/example/demo/dto/TaskRequest.java
+++ src/main/java/com/example/demo/dto/TaskRequest.java
@@ -1,6 +1,7 @@
 package com.example.demo.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -14,13 +15,16 @@ public class TaskRequest {
 
     @NotBlank(message = "name can not be blank")
     @JsonProperty("name")
+    @Schema(example = "Mission")
     private String name;
 
     @JsonProperty("description")
+    @Schema(example = "Protect John C.")
     private String description;
 
     @NotNull(message = "date can not be null")
     @JsonProperty("date")
+    @Schema(example = "1984-06-30")
     private LocalDate date;
 
 }
